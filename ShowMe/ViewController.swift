11577456b591375ac658a87967c1fe8bbff3e394
Jason Villegas <jaso3570@rackspace.com>
2018-09-08 19:31:28 -0500

    Testing

--- ShowMe/ViewController.swift
+++ ShowMe/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-          print("test")
+          print("tests")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
