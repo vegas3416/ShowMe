1bdb45a3a994a8e2870c94803ed5e122476ec8c6
Jason Villegas <jaso3570@rackspace.com>
2018-09-08 19:19:31 -0500

    Initial Commit

---
+++ ShowMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShowMe
+//
+//  Created by Jason Villegas on 9/8/18.
+//  Copyright © 2018 Jason Villegas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
